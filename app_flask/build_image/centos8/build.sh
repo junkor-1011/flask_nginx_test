4c024142e0c6a1b40dde06189c3c5f08ce352d73
test-user <test@email.com>
2020-03-29 16:37:58 +0900

    update setting

--- app_flask/build_image/centos8/build.sh
+++ app_flask/build_image/centos8/build.sh
@@ -8,7 +8,7 @@ cd $SCRIPT_DIR
 # symbolicの実体を辿ってimageをbuild
 # ref: https://blog.kkty.jp/entry/2019/06/16/214951
 tar -czh . | docker build \
-        -t app_flask_test_amazoncorretto8 \
-        --build-arg BASE_IMAGE=amazoncorretto:8 \
+        -t app_flask_test_centos8 \
+        --build-arg BASE_IMAGE=centos:8 \
         --build-arg USER_UID=1000 \
         - 
